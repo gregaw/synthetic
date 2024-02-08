416e29172150ed5f0a743c8d1dcfdbc9055dc1e7
Grzegorz Gawron <grzegorzgawron@gmail.com>
2024-02-08 16:31:59 +0100

    added the mbi func returning an error

--- synthetic/func.py
+++ synthetic/func.py
@@ -1,4 +1,11 @@
+from mbi import FactoredInference
+
 
 def boo(x):
     print("boo")
     return x * 2
+
+
+def mbi_func():
+    fi = FactoredInference(None, None, None, None)
+    return 2
--- tests/test_func.py
+++ tests/test_func.py
@@ -1,5 +1,13 @@
-from synthetic.func import boo
+from synthetic.func import boo, mbi_func
 
 
 def test_boo():
     assert boo(3) == 6
+
+
+def test_mbi_func():
+    """
+    This is a test for mbi_func, it should return an exception.
+    :return:
+    """
+    assert mbi_func() == 2
